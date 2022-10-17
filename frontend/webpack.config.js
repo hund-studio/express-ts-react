0ec79160ddaeb2506e6fff38bc3f8011f13522ec
Ernesto Bellei <ernesto@hund.studio>
2022-10-17 14:51:11 +0200

    added support for typescript tsconfig alias path

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./../config/webpack.config");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = merge(commonConfig, {
 	entry: path.join(__dirname, "index.tsx"),
 	output: {
 		path: path.join(__dirname, "../public/dist"),
 		filename: "client.js",
+	},
+	resolve: {
+		plugins: [
+			new TsconfigPathsPlugin({
+				configFile: path.join(__dirname, "tsconfig.json"),
+			}),
+		],
 	},
 });
